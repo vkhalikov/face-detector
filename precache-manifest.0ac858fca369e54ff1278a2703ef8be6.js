d3c50040f9c81acd96a97a31e62caf864a59a4cf
Vadim Khalikov <vkhalikov.work@gmail.com>
2019-11-30 01:21:04 +0300

    Updates

---
+++ precache-manifest.0ac858fca369e54ff1278a2703ef8be6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc2e4b65c6c97c72718120b814f00687",
+    "url": "/face-detector/index.html"
+  },
+  {
+    "revision": "0eb42f0310cadd95a1b4",
+    "url": "/face-detector/static/css/main.962a42f5.chunk.css"
+  },
+  {
+    "revision": "b6e815a1c4e01844b88a",
+    "url": "/face-detector/static/js/2.fe17af12.chunk.js"
+  },
+  {
+    "revision": "0eb42f0310cadd95a1b4",
+    "url": "/face-detector/static/js/main.a680fb01.chunk.js"
+  },
+  {
+    "revision": "96ba456c627c8f34982a",
+    "url": "/face-detector/static/js/runtime-main.3c85a41c.js"
+  }
+]);
